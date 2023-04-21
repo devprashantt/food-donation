6623c847f2079b07b78dbe484755be0216ac46cd
Aditya <123456789+odin@users.noreply.github.com>
2023-04-21 18:06:18 +0530

    login signup

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import './login.css';
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    
+  <div class="container">
+  
+  <div class="wrapper">
+    <form>
+      <h1>Login</h1>
+      <p>If you are already a member, easily log in</p>
+      <input type="email" id="email" placeholder="Email"></input>
+      
+      <input type="password" id="password" placeholder="Password"></input>
+        {/* <img src="https://cdn.pixabay.com/photo/2016/12/18/11/04/eye-1915454_960_720.png"></img> */}
+      
+      
+      <a href="#">Forgot my password</a>
+      <button type="submit" id="login-btn" >Log in</button>
+      <div class="or">
+        <hr></hr>
+        OR
+        <hr></hr>
+      </div>
+      <button class="google-btn">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-google" viewBox="0 0 16 16">
+          <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z"/>
+        </svg>
+        Login with Google
+      </button>
+      <div class="register">
+        <p>If you don't have an account</p>
+        <Link to={"/signup"}><button class="register-btn" >Register</button></Link>
+      </div>
+    </form>  
+
+  </div>
+  
+  <div class="main-img">
+   
+  </div>
+</div>
+
+
+ 
+    
+  );
+};
 
 export default Login;
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,14 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './signup.css';
 
 const Signup = () => {
   return (
-    <div>
-      Signup or{" "}
-      <Link exact to="/login">
-        Login
-      </Link>
-    </div>
+    
+    <div class="signup_container">
+        <div class="signup_main-img">
+          
+        </div>
+  
+  <div class="signup_wrapper">
+    <form>
+      <h1>Sign-Up</h1>
+      <p>Create your free account on Food-donation</p>
+      <input type="text" id="name" placeholder="Name"></input>
+      <input type="email" id="email" placeholder="Email"></input>
+      
+      <input type="password" id="password" placeholder="Password"></input>
+        
+      <input type="text" id="phone" placeholder="Phone"></input>
+      
+      
+
+      <button type="submit" id="signup-btn">Sign Up</button>
+      
+      <div class="login">
+        <p>Already have an account?</p>
+        <Link to={"/login"}><button class="login-btn" >Login</button></Link>
+      </div>
+    </form>  
+    
+
+  </div>
+  
+
+</div>
   );
 };
 
